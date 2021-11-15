fde4259c52560f6bd6930404ae8f703096f57839
WilliamDenega <williamdenega@gmail.com>
2021-11-15 17:04:55 -0500

    commit tree a1eb0be87880bd0e7841bc81f3f39e6c008dbe75

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-function saturdayFun(activity="roller-skate") {
-  return `This Saturday, I want to ${activity}!`
+function saturdayFun(activity="roller-skate"){
+  return `This Saturday, I want to ${activity}!`;
 }
 
-let mondayWork = function(activity="go to the office") {
-  return `This Monday, I will ${activity}.`
+function mondayWork(activity="go to the office"){
+  return `This Monday, I will ${activity}.`;
+
 }
 
-let wrapAdjective = function(style="*") {
-  return function(adjective="special") {
+function wrapAdjective(style='*'){
+  return function(adjective="special"){
     return `You are ${style}${adjective}${style}!`
   }
+
+
+
 }
